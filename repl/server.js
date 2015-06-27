555f733562ed672bff0a27c9562ef61b9849dff8
shenyulin <460457081@qq.com>
2015-06-27 17:10:40 +0800

    nodejs创建一个简单的服务器

--- repl/server.js
+++ repl/server.js
@@ -9,9 +9,10 @@ function waiter(req,res){
     //获取客户端请求的信息
     var url=req.url;
    if(url=="/menu"){
-       res.end('<a href="http://nodeapi.ucdok.com/#/api/">nodejsAPI</a><a href="dagou100.com">大狗商城</a>')
+       var str='<a href="http://nodeapi.ucdok.com/#/api/">nodejsAPI</a><br/><br/><a href="dagou100.com">大狗商城</a>';
+       res.end(str);
    }else{
-       res.end(url);
+       res.end("在url后输入menu试试");
    }
 }
 
